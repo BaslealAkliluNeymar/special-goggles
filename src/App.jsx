415cae48e2d10136934296f915dab08fe7aa21ab
Basleal Aklilu Lidetu <85111188+BaslealAkliluNeymar@users.noreply.github.com>
2023-09-21 17:18:12 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Nav from "./components/Nav"
+import One from "./components/One"
+const App = () => {
+  return (
+    <div className="p-0 m-0">
+      <Nav />
+      <One />
+      <One />
+    </div>
+  )
+}
+
+export default App
